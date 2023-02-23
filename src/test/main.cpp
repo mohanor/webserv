854784a89f06602a6913e8116ca08bddd7312014
Mohammed Atef <matef@e1r9p13.1337.ma>
2023-02-23 21:02:56 +0100

    conf file grammar

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include "../main/webserv.hpp"
 
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+
 #define PORT 80
+
+using namespace std;
 
 int main(int ac, char **av)
 {
@@ -17,6 +27,62 @@ int main(int ac, char **av)
     }
 
     std::cout << "Config file: --> " << config_file << " <--" << std::endl;
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("socket");
+        return 1;
+    }
+
+    int bind_status = bind(sockfd, NULL, 0);
+    if (bind_status < 0)
+    {
+        perror("bind");
+        return 1;
+    }
+
+    int listen_status = listen(sockfd, 0);
+    if (listen_status < 0)
+    {
+        perror("listen");
+        return 1;
+    }
+
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+
+    int client_fd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (client_fd < 0)
+    {
+        perror("accept");
+        return 1;
+    }
+
+    char buf[1024];
+    int read_status = read(client_fd, buf, sizeof(buf));
+    if (read_status < 0)
+    {
+        perror("read");
+        return 1;
+    }
+
+    std::cout << "Read " << read_status << " bytes" << std::endl;
+    std::cout << "Read: " << buf << std::endl;
+
+    int write_status = write(client_fd, buf, read_status);
+    if (write_status < 0)
+    {
+        perror("write");
+        return 1;
+    }
+
+    std::cout << "Wrote " << write_status << " bytes" << std::endl;
+
+    close(client_fd);
+    close(sockfd);
+
+    
 
 
     return 0;
