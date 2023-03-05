a12be5a18902e069dd503d595c2bad3083f79694
yoelhaim <youssefelhaimer8@gmail.com>
2023-03-05 22:28:18 +0100

    header config file class  (data type member function)

--- parsing/conf/ConfigParser.hpp
+++ parsing/conf/ConfigParser.hpp
@@ -6,7 +6,7 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 19:30:47 by yoelhaim          #+#    #+#             */
-/*   Updated: 2023/03/02 23:09:42 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2023/03/05 17:10:39 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,72 +16,45 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <map>
 #include <string>
 #include "../request/Request.hpp"
 #include "Server.hpp"
 #include "Http.hpp"
+#include "../../configuration/Configuration.hpp"
 using namespace std;
-
-enum e_token
-{
-    OPEN_CURLY,
-    CLOSE_CURLY,
-    SEMI_COLON,
-    WORD,
-    DIRECTIVE,
-    CONTEXT,
-    WSPACE,
-    END
-};
-
 
 class ConfigParser
 {
 private:
     string _lines;
     size_t _index;
+    size_t _lenght_server;
     vector<string> _data;
-    vector< pair<string, int> > _tokens;
-    vector<Server> _server;
+    vector<pair<string, int> > _tokens;
     
 public:
     ConfigParser();
     ConfigParser(string content);
-    void printData();
     ~ConfigParser();
-
-    string getLineParses(vector<string> data);
+    void printData();
+    void initalConfig(string content);
 
     void parseLine(string line);
-
     void checkWord(string  line);
-
-    // !! open and close curly brackets
     void checkOpenCurly(string  line);
     void checkCloseCurly(string  line);
-    
-    // !! Punctuation
     void checkPunctuation(string  line);
-    
-    // !! wspace
     void skipSpaces(string line);
-
-    // !! tokenez line  and check errors syntax 
+    void checkNewLine(string line);
      void tokenez();
-    //  !! check  Context
     vector<string>::iterator  checkContext2(vector<string>::iterator it);
     void checkContext();
 
     void pushTokinez(int currentIndex, string context);
     void pushSemiCurly(string type);
-    // !! check  Directive
     void checkDirectives(string data);
     void pushDirective(string directive);
-     
-    // !! check  errors log
     void errorLogs(string titleError);
-    // !! syntax error
     void synaxError();
     void checkSynatxCurly();
     void checkSyntaxMain();
@@ -89,11 +62,10 @@ public:
     void checkSyntaxDirective();
     bool checkSyntaxDirectiveCondition(size_t index);
     void checkSyntaxCemiColom();
+    void checkSyntackInServer(int currentIndex,int index);
+    void checkSynaxDirective();
+    void checkSyntaxMethod(size_t index);
     size_t lengthDirective(size_t index);
-    void checkMaxTwo(size_t index);
-    // ??Check and  push to Server
-    void   insertServer();
-    void checkAndInitail(size_t index, int indexServer,vector<Server> _server);
-    void addToServer(size_t index, int indexServer,vector<Server> _server);
-    size_t numberServer() const;
+    void  lenghtServer(int index);
+  
 };
