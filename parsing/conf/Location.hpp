3e63930b1c08f0ce596720024a201ecd8ef296bf
yoelhaim <youssefelhaimer8@gmail.com>
2023-03-05 22:29:51 +0100

    webserv :tada:

---
+++ parsing/conf/Location.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Location.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/05 16:14:00 by yoelhaim          #+#    #+#             */
+/*   Updated: 2023/03/05 16:57:06 by yoelhaim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+class Location
+{
+private:
+   string _return;
+public:
+    Location();
+    ~Location();
+};
--- src/test/main.cpp
+++ src/test/main.cpp
@@ -11,7 +11,7 @@ int main(int ac, char **av)
 
     ConfigParser config(getFileContent("./conf/default.conf"));
     // cout << "sddd\n";
-    config.printData();
+    // config.printData();
 
     // cout << r.serialize();
 
