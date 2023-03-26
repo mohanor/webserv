ed6cc9e36424dca6d62da04a38333f92e6b072b6
vito2carlos <younesselkhadar@gmail.com>
2023-03-26 08:11:27 +0000

    parsing cgi_info is the next step

--- src/cgi/cgi.hpp
+++ src/cgi/cgi.hpp
@@ -2,6 +2,7 @@
 
 #include "../methods/method.hpp"
 #include <unistd.h>
+#include <algorithm>
 using namespace std;
 
 class CGI
@@ -9,9 +10,11 @@ class CGI
     private:
         vector<string>  _env;
         string          _body;
+        string          _scriptName;
 
     public:
-        CGI(Request request, Server server,string body);
+        CGI(Request request, Server server,string url);
         char **_envToChar();
+        char **getScriptName(Request request, Server server, string url);
 
 };
--- src/methods/get.hpp
+++ src/methods/get.hpp
@@ -6,13 +6,14 @@
 /*   By: yel-khad <yel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 00:00:00 by yel-khad          #+#    #+#             */
-/*   Updated: 2023/03/24 00:00:01 by yel-khad         ###   ########.fr       */
+/*   Updated: 2023/03/26 02:15:48 by yel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 # include "method.hpp"
+# include "../server/Worker.hpp"
 
 class Get : public Method
 {
